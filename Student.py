44bb372f642fce0e9dc674d7008394d12a141e2b
Vishnutk2001 <t.k.vishnu2001@gmail.com>
2022-04-05 10:58:10 +0530

    student completed

--- Student.py
+++ Student.py
@@ -1,4 +1,23 @@
 from flask import Flask, render_template,request
+import sqlite3
+
+data = sqlite3.connect("studentdata.db",check_same_thread=False)
+table = data.execute("select name from sqlite_master where type='table' and name='student' ").fetchall()
+if table!=[]:
+    print("Table already exists")
+
+else:
+    data.execute('''create table student(
+                                        id integer primary key autoincrement,
+                                        Name text,
+                                        branch text,
+                                        rollno integer,
+                                        admno integer,
+                                        DOB text,
+                                        pass text,
+                                        semester text
+                                        );''')
+    print("Table created")
 
 app = Flask(__name__)
 
@@ -25,7 +44,19 @@ def Student_Registration_page():
         print(getSem)
         print(getPass)
         print(getConpass)
-        return "success"
+        try:
+            query = "insert into student(Name,branch,rollno,admno,DOB,pass,semester)\
+                     values('"+getName+"','"+getBranch+"',"+getRollno+","+getAdmno+",'"+getDOB+"','"+getSem+"','"+getPass+"')"
+            data.execute(query)
+            print(query)
+            data.commit()
+            data.close()
+            print("Inserted successfully")
+        except Exception as err:
+            print("error occured",err)
+
+
+
     return render_template("register.html")
 
 @app.route('/search')
